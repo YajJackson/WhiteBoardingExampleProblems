35438fd9c6bf81fa7d34f7197e6ec48b178e9051
Jay Jackson <jaynickjackson@gmail.com>
2017-11-09 11:05:18 -0500

    added BasicSetOne problem 3

---
+++ BasicSetOne/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicSetOne
+{
+    class Person
+    {
+        public Person(string first, string last, int age)
+        {
+            this.FirstName = first;
+            this.LastName = last;
+            this.Age = age;
+        }
+        
+        public Person(string first, int age)
+        {
+            this.FirstName = first;
+            this.Age = age;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+    }
+}
--- BasicSetOne/Program.cs
+++ BasicSetOne/Program.cs
@@ -18,6 +18,7 @@ namespace BasicSetOne
                 return first + " " + last;
             }
 
+
             // 2) Create a method that accepts two numbers as parameters and then returns
             //    their product
 
@@ -25,6 +26,16 @@ namespace BasicSetOne
             {
                 return numOne * numTwo;
             }
+
+
+            // 3) Create a Person class that has three properties: FirstName, LastName, and Age.
+            //    Person should have two constructors: one that requires all properties to be set,
+            //    and an overloaded constructor that only requires a firstName and age.
+            //    Instantiate two Person objects form the Person class.
+
+            Person jay = new Person("Jay", "Jackson", 22);
+            Person paul = new Person("Paul", 83);
+
         }
     }
 }
